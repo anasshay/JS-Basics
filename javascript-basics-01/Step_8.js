6a8e4209affe3b23da27c4fdb6ba04eca435968c
Anasshay <anasshaydar5@gmail.com>
2021-11-25 17:07:13 +0200

    Basics 01 done

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,10 @@
+function onClick() {
+    var age = document.getElementById('age').value;
+
+    if (age >= 18){
+        alert('You are over 18');
+    }
+    else {
+        alert('You are under 18');
+    }
+  }
