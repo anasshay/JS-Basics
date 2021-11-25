4ae501c6696ab25ad9abcd1aa49a41624124d20d
Anasshay <anasshaydar5@gmail.com>
2021-11-25 16:24:08 +0200

    Step 4 done

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = prompt("Enter your name: ");
+var surname = prompt("Enter your last name: ");
+var city = prompt("Enter your city: ");
+
+alert ("Name: " + name + "\nSurname: " + surname + "\nCity: " + city);
