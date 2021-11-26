6f413100a303aacb1ee00c4e85c4da4b80107898
Anasshay <anasshaydar5@gmail.com>
2021-11-26 22:00:55 +0200

    Done 03

--- javascript-basics-03/step 4/script.js
+++ javascript-basics-03/step 4/script.js
@@ -3,6 +3,9 @@ var pass2Val = document.getElementById('confirmation');
 
 function validate() {
     if (pass1Val.value != pass2Val.value) {
-        Array.from(document.querySelectorAll('input')).forEach (element => element.style.borderColor = 'red');
+        Array.from(document.querySelectorAll('input')).forEach(element => element.style.borderColor = 'red');
     }
-  }
+    else {
+        Array.from(document.querySelectorAll('input')).forEach(element => element.style.borderColor = 'initial');
+    }
+}
