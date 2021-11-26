6968b74fe8557842238d0dc8a37fe01b3421b1c7
Anasshay <anasshaydar5@gmail.com>
2021-11-26 22:22:54 +0200

    Fix 03

--- javascript-basics-03/step 3/script.js
+++ javascript-basics-03/step 3/script.js
@@ -1,9 +1,13 @@
+// var color = document.querySelectorAll('.color');
+// for (let i = 0; i < color.length; i++) {
+//     color[i].addEventListener('click', function() {
+//         div.style.color = color[i].classList[1];
+//     });
 
-var color = document.querySelectorAll('.color');
+// }
+
 var div = document.getElementById('text');
-for (let i = 0; i < color.length; i++) {
-    color[i].addEventListener('click', function() {
-        div.style.color = color[i].classList[1];
-    });
-    
-}
+document.addEventListener('click', function (event) {
+    var val = event.target.classList[1];
+    div.style.color = val;
+});
