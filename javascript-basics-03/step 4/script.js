79012dc079d11ffd2094b7c0e63f2032a55db2dd
Anasshay <anasshaydar5@gmail.com>
2021-11-26 21:53:48 +0200

    3_step 4 done

---
+++ javascript-basics-03/step 4/script.js
@@ -0,0 +1,8 @@
+var pass1Val = document.getElementById('password');
+var pass2Val = document.getElementById('confirmation');
+
+function validate() {
+    if (pass1Val.value != pass2Val.value) {
+        Array.from(document.querySelectorAll('input')).forEach (element => element.style.borderColor = 'red');
+    }
+  }
