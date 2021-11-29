2625b1a0f5e3d907d87daeee6ea919ca4a36a713
Anasshay <anasshaydar5@gmail.com>
2021-11-29 12:38:08 +0200

    createLanguageArray

--- javascript-basics-03/step 3/script.js
+++ javascript-basics-03/step 3/script.js
@@ -1,13 +1,13 @@
-// var color = document.querySelectorAll('.color');
-// for (let i = 0; i < color.length; i++) {
-//     color[i].addEventListener('click', function() {
-//         div.style.color = color[i].classList[1];
-//     });
 
-// }
 
 var div = document.getElementById('text');
-document.addEventListener('click', function (event) {
-    var val = event.target.classList[1];
-    div.style.color = val;
+// document.addEventListener('click', function (event) {
+//     var val = event.target.classList[1];
+//     div.style.color = val;
+// });
+
+document.querySelectorAll('.color').forEach (item => {
+    item.addEventListener('click', function() {
+        div.style.color = item.classList[1];
+    });
 });
